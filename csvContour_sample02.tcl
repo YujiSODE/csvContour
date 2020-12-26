036d35629a55a1ac10945729f6c8dc9f367828bc
Yuji Sode <yuji.sode@gmail.com>
2020-12-26 15:01:59 +0900

    Add files via upload

---
+++ csvContour_sample02.tcl
@@ -0,0 +1,19 @@
+#csvContour_sample02.tcl
+#sample codes for `csvContour.tcl`
+source -encoding utf-8 csvContour.tcl;
+#
+set map {};
+append map "0,0,0,0,0,0,0,0,0,0";
+append map "\n1,1,1,1,1,2,2,2,2,2";
+append map "\n1,1,1,1,1,2,2,2,2,2";
+append map "\n1,1,1,1,1,2,2,2,2,2";
+append map "\n1,1,1,1,1,2,2,2,2,2";
+append map "\n0,0,0,3,3,3,3,3,2.5,2.5";
+append map "\n0,0,0,3,3,3,3,3,2.5,2.5";
+append map "\n0,0,0,3,0,0,0,3,2.5,2.5";
+append map "\n0,0,0,3,0,0,0,3,2.5,2.5";
+append map "\n0,0,0,3,0,4,0,3,0,0";
+append map "\n0,0,0,3,0,4,0,3,0,0";
+append map "\n0,0,0,3,0,4,0,3,0,0";
+#
+puts stdout [csvContour $map 100];
